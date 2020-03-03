ea0cf403db55e0daff83132d8685598aa207b6c3
paxexterminatus <Freeman100%>
2020-03-03 22:58:01 +0300

    update footer - adaptive

--- code/resources/components/App.vue
+++ code/resources/components/App.vue
@@ -58,7 +58,7 @@
                 </div>
 
                 <div class="footer-main-copy">
-                    <div>© Nike, Inc.,2020 Все права защищены.</div>
+                    <div class="copy-rights">© Nike, Inc., 2020 Все права защищены.</div>
                     <div>Каталог</div>
                     <div>Условия использования</div>
                     <div>Условия продажи</div>
@@ -81,46 +81,3 @@
         }
     }
 </script>
-
-<style lang="scss">
-    .footer-main {
-        height: 529px;
-        background: #F4F4F4;
-        padding: 88px 0 81px 0;
-        .container {
-            height: 100%;
-        }
-    }
-    .footer-main-info {
-        display: flex;
-        justify-content: space-between;
-        & > * {
-            flex: 1;
-        }
-    }
-    .footer-main-links {
-        display: flex;
-        flex-direction: column;
-        margin-right: 20px;
-        label, a {
-            font-size: 20px;
-            line-height: 130%;
-            margin-bottom: 1rem;
-        }
-        label {
-            font-weight: bold;
-        }
-    }
-    .footer-main-icons {
-        display: flex;
-        justify-content: center;
-    }
-    .footer-main-copy {
-        margin-top: 118px;
-        display: flex;
-        font-size: 12px;
-        line-height: 130%;
-        color: #999999;
-        justify-content: space-between;
-    }
-</style>
